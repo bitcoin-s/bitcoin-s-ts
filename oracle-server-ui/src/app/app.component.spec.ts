990934c41a1a21965c778cbc412780aa0d688f85
Ivan <erickson.ivan@gmail.com>
2021-09-10 09:07:15 -0600

    Fix test

--- oracle-server-ui/src/app/app.component.spec.ts
+++ oracle-server-ui/src/app/app.component.spec.ts
@@ -16,11 +16,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'oracle-server-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('oracle-server-ui');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
