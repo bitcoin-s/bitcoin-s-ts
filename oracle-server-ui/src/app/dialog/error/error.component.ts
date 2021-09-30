5aaf9215ebc30223020a50111af0dd202a465575
Ivan <erickson.ivan@gmail.com>
2021-09-30 11:46:08 -0600

    Add ErrorDialog, oracle-server-util

---
+++ oracle-server-ui/src/app/dialog/error/error.component.ts
@@ -0,0 +1,20 @@
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+export interface ErrorDialogContent {
+  title: string
+  content: string
+  params: any // { key: value }
+  // action: string
+}
+
+@Component({
+  selector: 'error-dialog',
+  templateUrl: './error.component.html',
+  styleUrls: ['./error.component.scss']
+})
+export class ErrorDialogComponent {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: ErrorDialogContent) { }
+}
