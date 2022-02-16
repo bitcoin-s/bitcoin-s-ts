579052e1aea4a16187a6cc3c1dc9b997b1f4c5a8
Ivan <erickson.ivan@gmail.com>
2022-02-16 14:30:53 -0700

    Fix tests, separate build steps

--- wallet-server-ui/src/app/app.component.spec.ts
+++ wallet-server-ui/src/app/app.component.spec.ts
@@ -16,16 +16,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'wallet-server-ui'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('wallet-server-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('wallet-server-ui app is running!');
-  });
 });
