69c33e24c49478cbcb073fe361df0dc6032a3c00
Ivan <erickson.ivan@gmail.com>
2022-03-14 09:33:34 -0600

    Updates before Bitcoin 2022 feature branch

---
+++ wallet-server-ui/src/service/localstorage.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core'
+
+
+@Injectable({ providedIn: 'root' })
+export class LocalStorageService {
+
+  constructor() {}
+
+  get(key: string): string|null {
+    return localStorage.getItem(key)
+  }
+
+  getObject(key: string): any|null {
+    const s = localStorage.getItem(key)
+
+    if (s) {
+      try {
+        const obj = JSON.parse(s)
+        return obj
+      } catch (err) {
+        console.error('error parsing localStorage key', key, err)
+      }
+    }
+    return null
+  }
+
+  set(key: string, value: any) {
+    if (value) {
+      const stringValue = JSON.stringify(value)
+      localStorage.setItem(key, stringValue)
+    }
+  }
+
+  clear(key: string) {
+    localStorage.removeItem(key)
+  }
+
+}
