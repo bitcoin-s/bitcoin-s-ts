5db96b591cd0aa0284997b198f21a01885fa45d8
GreyMcCarthy <105063806+GreyMcCarthy@users.noreply.github.com>
2022-06-22 13:40:38 -0400

    Now checks if the bitcoin-s-server is in wallet-electron-ts and runs only if it finds a file with bitcoin-s-server in part of its name

--- build-wallet-electron.sh
+++ build-wallet-electron.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-npm i && npm run build 
-cd wallet-electron-ts 
-npm i && npm run build 
-echo "Want to start the server in dev mode? Run cd wallet-electron-ts && npm run start"
+cd wallet-electron-ts
+for name in *bitcoin-s-server*; do
+	if [ -f "$name" ]; then 
+		cd ..
+		npm i && npm run build 
+		cd wallet-electron-ts 
+		npm i && npm run build 
+		echo "Want to start the server in dev mode? Run cd wallet-electron-ts && npm run start"
+	else 
+		cd..
+		echo "need to put bitcoin-s-server.zip file in ~/bitcoin-s-ts/wallet-electron-ts. 
+		To do so go to your bitcoin-s node and inside it Run sbt appServer/universal:packageBin
+		then Run cd app/server/target/universal 
+		then Run cp bitcoin-s-server-1.9.1-87-f210d9d9-SNAPSHOT.zip ~/bitcoin-s-ts/wallet-electron-ts"
+
+	fi
+done
