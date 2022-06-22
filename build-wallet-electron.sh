0c43e6bb82cdf582884beae8cdf0cf86b8965b76
GreyMcCarthy <105063806+GreyMcCarthy@users.noreply.github.com>
2022-06-22 11:07:17 -0400

    Removed semi colons

--- build-wallet-electron.sh
+++ build-wallet-electron.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npm i && npm run build; 
-cd wallet-electron-ts; 
-npm i && npm run build; 
+npm i && npm run build 
+cd wallet-electron-ts 
+npm i && npm run build 
 echo "Want to start the server in dev mode? Run cd wallet-electron-ts && npm run start"
