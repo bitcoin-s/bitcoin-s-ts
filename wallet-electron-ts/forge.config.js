a6a2e15878000f150c5f74328a8bafb20e0a0c4c
user411 <erickson.ivan@gmail.com>
2022-06-28 09:47:27 -0600

    Electron Config for multi-platform build (#158)
    * Externalize Krystal Bull forge config, add mac signing and notarization
    
    * Use Team Id with notarize
    
    * Add appBundleId
    
    * Use keychain to notarize
    
    * Test sign with keychain
    
    * Switch back to env var identity
    
    * Switch back to env var identity
    
    * Use signing_temp keychain
    
    * Add keychain to notarize vs signing
    
    * Add keychain name back to signing
    
    * electron sign gatekeeper-access: false
    
    * Platform specific config
    
    * Add wallet forge.config.js
    
    * Wallet use forge.config
    
    * Roll up versions, test entitlements
    
    * Check in package-lock.jsons
    
    * Add entitlements.plist
    
    * Turn off default dmg build
    
    * Update windows electron forge config
    
    * Remove experimental entitlements
    
    * Strip experimental entitlements
    
    * Electron 19.0.6
    
    * Fix plist

---
+++ wallet-electron-ts/forge.config.js
@@ -0,0 +1,144 @@
+const os = require('os')
+
+const p = os.platform() // 'darwin', 'linux', 'win32'
+const DARWIN = 'darwin'
+const LINUX = 'linux'
+const WIN32 = 'win32'
+
+// See https://stackoverflow.com/questions/69046910/electron-forge-securely-add-appleid-and-password
+
+// const { utils: { fromBuildIdentifier } } = require('@electron-forge/core');
+
+// module.exports = {
+//   buildIdentifier: process.env.IS_BETA ? 'beta' : 'prod',
+//   packagerConfig: {
+//     appBundleId: fromBuildIdentifier({ beta: 'com.beta.app', prod: 'com.app' })
+//   }
+// }
+
+// See https://github.com/electron/electron-notarize
+
+// TODO : Set this from a flag
+const appType = 'development' //, 'distribution'
+const keychain = 'signing_temp.keychain' // keychain name, could pass in
+const appBundleId = 'org.bitcoins.suredbitswallet'
+
+function getIcon() {
+  switch (p) {
+    case 'darwin':
+      return 'assets/bitcoin-S.icns'
+    case 'linux':
+      return 'assets/bitcoin-S.png'
+    case 'win32':
+      return 'assets/bitcoin-S.ico'
+    default:
+      throw new Error(
+        'forge.config.js error: your OS is not supported. Supported OS are: darwin, linux, win32',
+      )
+  }
+}
+
+const CONFIG = {
+  packagerConfig: {
+    "icon": getIcon(),
+    "executableName": "wallet-electron-ts"
+  },
+  // electronRebuildConfig: {},
+  makers: [ {
+    "name": "@electron-forge/maker-squirrel",
+    "config": {
+      "name": "wallet-electron-ts"
+    }
+  },
+  {
+    "name": "@electron-forge/maker-zip",
+    "platforms": [
+      "darwin",
+      "linux"
+    ]
+  },
+  // {
+  //   "name": "@electron-forge/maker-dmg",
+  //   "config": {
+  //     "iconSize": 128
+  //   }
+  // },
+  {
+    "name": "@electron-forge/maker-deb",
+    "config": {
+      "options": {
+        "name": "suredbits-wallet",
+        "productName": "Suredbits Wallet",
+        "icon": "assets/bitcoin-S.png",
+        "homepage": "https://suredbits.com",
+        "maintainer": "Suredbits"
+      }
+    }
+  }],
+  publishers: [],
+  plugins: [[
+    "@electron-forge/plugin-webpack",
+    {
+      "mainConfig": "./webpack.main.config.js",
+      "renderer": {
+        "config": "./webpack.renderer.config.js",
+        "entryPoints": [
+          {
+            "html": "./src/index.html",
+            "js": "./src/renderer.ts",
+            "name": "main_window",
+            "preload": {
+              "js": "./src/preload.ts"
+            }
+          }
+        ]
+      }
+    }
+  ]],
+  // hooks: {},
+  // buildIdentifier: 'my-build'
+}
+
+/** Platform specific config changes */
+
+if (p === DARWIN) {
+  // Add macos signing and notarization
+  // See https://github.com/electron/osx-sign
+  if (process.env.APP_SIGNING_ID) {
+    console.debug('Signing app with APP_SIGNING_ID:', process.env.APP_SIGNING_ID)
+    CONFIG.packagerConfig.osxSign = {
+      "identity": process.env.APP_SIGNING_ID,
+      "identityValidation": true,
+      "keychain": keychain,
+      "type": appType,
+      // "gatekeeper-assess": false, // This flag is problematic. Doesn't want to sign successfully without it, app doesn't seem to open with it
+      "hardened-runtime": true,
+      // TODO : Need to address signing issues in https://osxapps-ssl.itunes.apple.com/itunes-assets/Enigma112/v4/f7/35/cb/f735cb93-f916-de7c-2d25-f63464888437/developer_log.json?accessKey=1656241094_3099320763189474635_sq%2F2q9mZhc0FWHrKyij7yu3n5Fh0xtY9mJwhvdrWAFMCLCIRDdzMW11yuvFoxik8NgbdKL0BDAwpcSRgxhpkx7YxCQJc%2F4DPoX2knxgn9FtrlIfmDM60uKiagsTR5EHVrVQXxlDcCePPntOieCv%2FaaIFVPP1C%2BPyZNE%2BD5XjJiU%3D
+      "entitlements": "entitlements.plist",
+      "entitlements-inherit": "entitlements.plist",
+      "signature-flags": "library",
+      // This is going to be a pain as versions change
+      // "binaries": [],
+    }
+  }
+  // See https://github.com/electron/electron-notarize
+  if (process.env.NOTORIZE_APPLE_ID && process.env.NOTORIZE_APPLE_PW && process.env.NOTORIZE_APPLE_TEAM) {
+    console.debug('Notarizing app with NOTORIZE_APPLE_ID:', process.env.NOTORIZE_APPLE_ID, 'NOTORIZE_APPLE_TEAM:', process.env.NOTORIZE_APPLE_TEAM)
+    CONFIG.packagerConfig.osxNotarize = {
+      "appBundleId": appBundleId,
+      "appleId": process.env.NOTORIZE_APPLE_ID,
+      "appleIdPassword": process.env.NOTORIZE_APPLE_PW,
+      "ascProvider": process.env.NOTORIZE_APPLE_TEAM,
+      // May need to use keychain / keychainProfile https://github.com/electron/electron-notarize#safety-when-using-appleidpassword
+      "keychain": keychain,
+      "keychainProfile": process.env.NOTORIZE_APPLE_ID,
+    }
+  }
+} else if (p === LINUX) {
+  // Nothing to do here? May need to adjust icon
+} else if (p === WIN32) {
+  console.debug('TODO : update forge.config output for win32')
+  CONFIG.packagerConfig.executableName = "Suredbits Wallet"
+}
+
+module.exports = CONFIG
