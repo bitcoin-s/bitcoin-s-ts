42d59baddaa0d76434e5576e206002fdad83898a
GreyMcCarthy <105063806+GreyMcCarthy@users.noreply.github.com>
2022-06-28 15:02:29 -0400

    Bash file Suredbits desktop instillation (#155)
    * added bash file for install surebits desktop
    
    * added bash file to make suredbits desktop instillation easier
    
    * Added echo statement to make it clear how to start the server, and deleted npm start and npm make
    
    * Removed semi colons
    
    * Removed the new line at the end of the file
    
    * Now checks if the bitcoin-s-server is in wallet-electron-ts and runs only if it finds a file with bitcoin-s-server in part of its name
    
    * Now downloads the bitcoin-s-server.zip for mac
    
    * Now downloads the bitcoin-s-server for Win, Mac, or Linux
    
    * fixed problem download bitcoin-s-sever-Windiows.zip instead of bitcoin-s-server-Linux
    
    * added explination of what code does and fixed error in instructions for m1 mac if not working
    
    * removed check for unsupported OS and fixed so that mac m1 is accounted for
    
    * added npm run clean so don't get error on re-download, fixed discription of what the base file does, removed unnsescary things in the code
    
    * made it so checked for chip no OS when checking if m1 mac
    
    * added npm run make and removed AIX as operating system it checks for

---
+++ build-wallet-electron.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# downloads the bitcoin-s-server.zip file corresponding to your operating system 
+# then sets up Suredbits Wallets dependencies
+OS="`uname`"
+case $OS in
+  'Linux')
+   OS='Linux'
+    ;;
+  'WindowsNT')
+    OS='Windows'
+    ;;
+  'Darwin') 
+    OS='Mac'
+    ;;
+  'incompatable') ;;
+  *) ;;
+esac
+
+cd wallet-electron-ts
+
+Chip="`uname -m`"
+
+if [[ $Chip == 'arm64' ]]; then 
+	echo "you're on a M1 Mac need to generate bitcoin-s-sever.zip if have not already done so"
+elif [[ $OS == 'Mac' ]]; then 
+	curl -O -L https://github.com/bitcoin-s/bitcoin-s/releases/download/1.9.2/bitcoin-s-server-mac.os.x-1.9.2.zip
+elif [[ $OS == 'Windows' ]]; then
+	curl -O -L https://github.com/bitcoin-s/bitcoin-s/releases/download/1.9.2/bitcoin-s-server-windows.server.2019-1.9.2.zip
+elif [[ $OS == 'Linux' ]]; then
+	curl -O -L https://github.com/bitcoin-s/bitcoin-s/releases/download/1.9.2/bitcoin-s-server-linux-1.9.2.zip
+
+else 
+	echo "not supported OS"
+fi
+
+for name in *bitcoin-s-server*; do
+	if [ -f "$name" ]; then 
+		cd ..
+		npm run clean
+		npm i && npm run build 
+		cd wallet-electron-ts 
+		npm i && npm run build && npm run make
+		echo "Want to start the server in dev mode? Run cd wallet-electron-ts && npm run start"
+	else 
+		cd..
+		echo "
+		Retrieve bitcoin-s-server file from https://github.com/bitcoin-s/bitcoin-s releases/tag/1.9.2 need to put bitcoin-s-server.zip file in ~/bitcoin-s-ts/wallet-electron-ts. 
+		For Mac m1 if not on the github then need to generate it like so
+		go to your bitcoin-s node and inside it Run sbt appServer/universal:packageBin
+		then Run cd app/server/target/universal 
+		then Run cp <the zip file> ~/bitcoin-s-ts/wallet-electron-ts"
+
+	fi
+done
